a70d099f2b8fba715b54cb7e92ef5df59d907f40
yachna gupta <yachnagupta04@gmail.com>
2026-05-31 13:43:44 +0530

    .

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './public/index.html',
+    './public/src/**/*.{js,jsx,ts,tsx,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
